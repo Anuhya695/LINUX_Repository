4deb2667be0fc4751df4cd39863ef589b8b62ef9
Anuhya695 <anupendam1590@gmail.com>
2024-06-18 11:14:11 +0530

    FORLOOP

---
+++ Git Files/File3.py
@@ -0,0 +1,15 @@
+class Variable
+{
+   public static void main(String args[])
+   {
+    int a=10;
+    int b=50;
+    int c=30;
+    int Sum=a+b+c;
+    System.out.println(Sum);
+    int Diff=a-b-c;
+    System.out.println(Diff);
+    int Mul=b*a*c;
+    System.out.println(Mul);
+   } 
+}
